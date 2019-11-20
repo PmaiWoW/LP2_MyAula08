acabd038c8e4f06b96e48b78a6aca625f83f88ef
PmaiWoW <PmaiWoW@Gmail.com>
2019-11-20 14:50:15 +0000

    Add class exercise 1, ExtendString

---
+++ ExtendString/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ExtendString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "bippity, boppity, boop";
+            Console.WriteLine(s);
+            Console.WriteLine(s.ToRandomCase());
+        }
+    }
+}
---
+++ ExtendString/StringExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtendString
+{
+    public static class StringExtensions
+    {
+        public static string ToRandomCase(this string s)
+        {
+            StringBuilder sb = new StringBuilder();
+            Random rnd = new Random();
+
+            // Em ciclo foreach
+            // foreach (char c in s)
+            // {
+            //     if (rnd.NextDouble() > 0.5)
+            //     {
+            //         sb.Append(c.ToString().ToLower());
+            //     }
+            //     else
+            //     {
+            //         sb.Append(c.ToString().ToUpper());
+            //     }
+            // }
+
+            // Em ciclo for (mais rápido)
+            for(int i = 0; i < s.Length; i++)
+            {
+                //Em operador ternário
+                sb.Append(rnd.NextDouble() > 0.5 ?
+                    s[i].ToString().ToUpper() : s[i].ToString().ToLower());
+
+                // Em if/else tradicional
+                // if (rnd.NextDouble() > 0.5)
+                // {
+                //     sb.Append(s[i].ToString().ToLower());
+                // }
+                // else
+                // {
+                //     sb.Append(s[i].ToString().ToUpper());
+                // }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
